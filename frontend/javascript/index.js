2af1da19498131b5614e02dac72f3dad4415187a
John Alvin Ambalong <119690431+jambalong@users.noreply.github.com>
2024-10-05 22:45:42 -0700

    Add Info and Skills

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -5,3 +5,18 @@ import "$styles/syntax-highlighting.css"
 import components from "$components/**/*.{js,jsx,js.rb,css}"
 
 console.info("Bridgetown is loaded!")
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+let menuOpen = false;
+
+hamburger.addEventListener('click', () => {
+  if (menuOpen == false) {
+    navLinks.style.display = "block";
+    menuOpen = true;
+  }
+  else if (menuOpen == true) {
+    navLinks.style.display = "none";
+    menuOpen = false;
+  }
+});
